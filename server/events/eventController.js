c0f0d2849a0de74e3f385577cc54bfa8480e6144
Ryan Newton <newton1988@gmail.com>
2016-05-19 15:28:58 -0500

    Added preliminary createEvent() and getEvents() functions

--- server/events/eventController.js
+++ server/events/eventController.js
@@ -2,19 +2,29 @@ var Event = require('./eventModel.js');
 
 module.exports = {
   createEvent: function(req, res, next) {
-    var name       = req.body.name;
-    var location   = req.body.location;
-    var startTime  = req.body.startTime;
-    var endTime    = req.body.endTime;
-    var picUrl     = req.body.picUrl;
-    var categories = req.body.categories;
-    var usersAtt   = req.body.usersAtt;
-    var owners     = req.body.owners;
-    var description= req.body.description;
+    var newEvent = new Event({
+      event_name : req.body.name;
+      location   : req.body.location;
+      start_time : req.body.startTime;
+      end_time   : req.body.endTime;
+      pic_url    : req.body.picUrl;
+      categories : req.body.categories;
+      users_att  : req.body.usersAtt;
+      owners     : req.body.owners;
+      description: req.body.description;
+    });
+
+    newEvent.save(function (err, newEvent) {
+      if(err) { return console.error(err); }
+    });
   },
 
+  //To do: Accept an object argument and filter based on it
   getEvents: function() {
-
+    Event.find(function (err, events) {
+      if(err) { return console.error(err); }
+      return events;
+    })
   },
 
   deleteEvent: function() {
